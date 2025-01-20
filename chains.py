50536231cf95f0127b3156c4b0c122d1bd660c28
Nehlr1 <rahmahfuzurad@gmail.com>
2025-01-20 18:23:11 +0600

    Updated chains.py

--- chains.py
+++ chains.py
@@ -33,7 +33,29 @@ class ChatGroqChain:
         return result if isinstance(result, list) else [result]
     
     def create_email(self, job, links):
-        pass
+        prompt_email = PromptTemplate.from_template(
+            """
+            ### JOB DESCRIPTION:
+            {job_description}
+
+            ### INSTRUCTION:
+            You are Muhammad Ali, a business development executive at Somikoron AI. Somikoron AI is an AI & Software Consulting company dedicated to facilitating
+            the seamless integration of business processes through automated tools. 
+            Over our experience, we have empowered numerous enterprises with tailored solutions, fostering scalability, 
+            process optimization, cost reduction, and heightened overall efficiency. 
+            Your job is to write a cold email to the client regarding the job mentioned above describing the capability of Somikoron AI 
+            in fulfilling their needs.
+            Also add the most relevant ones from the following links to showcase Somikoron AI's portfolio: {link_list}
+            Remember you are Muhammad Ali, BDE at Somikoron AI. 
+            Do not provide a preamble.
+            ### EMAIL (NO PREAMBLE):
+
+            """
+        )
+
+        chain_email = prompt_email | self.chat_groq
+        result = chain_email.invoke(input={"job_description": str(job), "link_list": links})
+        return result.content
     
 if __name__ == "__main__":
     print(os.getenv("GROQ_API_KEY"))
